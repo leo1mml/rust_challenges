dc857075640ba13cadf75518d72f2ed02f010c93
Leonel Lima <mml_leo@hotmail.com>
2023-12-07 20:20:28 +0000

    Added base string to value

--- src/bin/convert_base.rs
+++ src/bin/convert_base.rs
@@ -23,6 +23,20 @@ mod convert_base {
         };
         return digit.to_string();
     }
+
+    pub fn base_string_to_value(digits: &str, base: i32) -> i32 {
+        if digits.is_empty() {
+            return 0;
+        }
+    let last_digit_value = digits
+        .chars()
+        .last()
+        .map(|n| n.to_digit(base as u32).unwrap_or(0) as i32);
+        let mut output = last_digit_value.unwrap_or(0);
+        let remainder_string = &digits[..digits.len() - 1];
+        output += base * base_string_to_value(remainder_string, base);
+        return output;
+    }
 }
 
 fn main() {
@@ -38,5 +52,13 @@ mod tests {
         assert_eq!("10", number_to_base_string(2, 2));
         assert_eq!("10", number_to_base_string(10, 10));
         assert_eq!("B", number_to_base_string(11, 16));
+    }
+
+    #[test]
+    fn convert_string_to_decimal() {
+        assert_eq!(10, base_string_to_value("10", 10));
+        assert_eq!(base_string_to_value("123", 10), 123);
+        assert_eq!(base_string_to_value("A1", 16), 161);
+        assert_eq!(base_string_to_value("101", 2), 5);
     }
 }
